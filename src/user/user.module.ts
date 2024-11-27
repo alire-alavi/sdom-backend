6cc7f3f87fb93d534d3a43f74816a2312a91aacd
alire-alavi <alirezaalavirazavi@yahoo.com>
2024-11-27 23:18:10 +0330

    [REMOVE] unused imports

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
-import { User } from './user.entity';
 import { DynamoDbUserRepository } from './dynamodb-user.repository';
 import { UserRepository } from './user.repository';
 import { UserController } from './user.controller';
