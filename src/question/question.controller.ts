2e4cf5a130c1d22175da7980830da7e04fad2f79
alire-alavi <alirezaalavirazavi@yahoo.com>
2024-11-30 23:25:50 +0330

    update: make questions routes public

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -2,16 +2,19 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { QuestionService } from './question.service';
 import { Question } from './question.entity';
 import { BatchValidateAnswerDto } from './dto';
+import { Public } from 'src/decorators/auth.decorator';
 
 @Controller('questions')
 export class QuestionController {
   constructor(private readonly questisonService: QuestionService) {}
 
+  @Public()
   @Get('random')
   async getRandomQuestions(): Promise<Question[]> {
     return this.questisonService.getRandomQuestions();
   }
 
+  @Public()
   @Post('validate')
   async validateAnswer(
     @Body() batchValidateAnswer: BatchValidateAnswerDto,
