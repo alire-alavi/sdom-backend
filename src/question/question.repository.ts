6c2e783bc2a9cedd034969ff1add48c7a265f5c9
alire-alavi <alirezaalavirazavi@yahoo.com>
2024-12-07 15:30:11 +0330

    question:remove limit from repsoitry findRandom

--- src/question/question.repository.ts
+++ src/question/question.repository.ts
@@ -3,5 +3,5 @@ import { Question } from './question.entity';
 export abstract class QuestionRepository {
   abstract findAll(): Promise<Question[]>;
   abstract findOne(id: string | number): Promise<Question>;
-  abstract findRandom(limit: number): Promise<Question[]>;
+  abstract findRandom(): Promise<Question[]>;
 }
